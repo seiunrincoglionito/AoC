d356f38903037dd96c0c3f20800cf8784bcf7ff6
Giovanni Romanò <3172379@phd.unibocconi.it>
2022-12-01 20:16:55 +0100

    Aggiunto funzione per il 2° problema del giorno

--- AoC1.jl
+++ AoC1.jl
@@ -21,3 +21,31 @@ print(maxx)
 end
 
 max_cal()
+
+
+function minmax_cal(max_counter::Int)
+    f = open("input.txt")
+
+    minmaxx = [-1, -1, -1]
+    t = 0
+    c = 1
+    for ln in eachline(f) 
+        if ln == ""
+            if c ≤ max_counter
+                minmaxx[c] = t
+                sort!(minmaxx)
+            elseif t > min(minmaxx)
+                minmax[1] =  t
+                sort!(minmaxx)
+            end
+            t = 0
+        else
+            t += parse(Int, ln)
+        end
+    end
+
+print(sum(minmaxx))
+
+end
+
+minmax_cal(3)

84e67039a01608ae319dbe23a2f0a0db30dbe6c9
beatrice.achilli <3146862@phd.unibocconi.it>
2022-12-03 14:59:08 +0100

    Problem 1 day 3 Bea

--- AoC3.jl
+++ AoC3.jl
@@ -23,4 +23,51 @@ function prob1Gio(filename::String)
     return sum
 end
 
-print(prob1Gio("input_3.txt"))
+println(prob1Gio("input_3.txt"))
+
+function split_comp(s::String)
+
+    s1 = s[1:Int(length(s)/2)]
+    s2 = s[Int(length(s)/2)+1 : Int(length(s))]
+    return s1, s2
+
+end
+
+function match_char(s1::String, s2::String)
+
+    for c in s1
+        if contains(s2, c)
+          return c
+        end
+    end
+
+end
+
+function priority(c::Char)
+
+    if isuppercase(c)
+        return (Int(c) - 38)
+    else 
+        return (Int(c) - 96)
+    end
+
+end
+
+function tot_priorities(input::String)
+    
+    f = open(input)
+    tot = 0
+
+    for ln in eachline(f)
+        s1, s2 = split_comp(ln)
+        c = match_char(s1, s2)
+        tot += priority(c)
+    end
+
+    return tot
+
+end
+
+println(tot_priorities("input_3.txt"))
+println(priority('p'))
+println(priority('L'))

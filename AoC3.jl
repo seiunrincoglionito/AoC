9b748ad5579b5f0359c0e266b703cf21de75d6b9
Giovanni Romanò <3172379@phd.unibocconi.it>
2022-12-03 14:28:16 +0100

    prob1Gio fixed and prob2Gio added

--- AoC3.jl
+++ AoC3.jl
@@ -3,8 +3,8 @@ Pkg.activate(".")
 Pkg.instantiate()
 
 
-function halfslice(a::Array)
-    [a[1:(end ÷ 2)], a[(end ÷ 2):end]]
+function halfslice(a::String)
+    [a[1:(end ÷ 2)], a[(end ÷ 2 + 1):end]]
 end
 
 function prob1Gio(filename::String)
@@ -13,10 +13,10 @@ function prob1Gio(filename::String)
     alphabet = 'a':'z'
     dict_l = Dict(alphabet[i] => i for i in 1:26)
     for ln in eachline(f)
-        h = halfslice(split(ln, ""))
+        h = halfslice(ln)
         i = intersect(h[1], h[2])
         for el in i
-            sum += dict_l[only(lowercase(el))] + 26 * isuppercase(only(el))
+            sum += (dict_l[only(lowercase(el))] + 26 * isuppercase(only(el)))
     
         end
     end
@@ -24,3 +24,20 @@ function prob1Gio(filename::String)
 end
 
 print(prob1Gio("input_3.txt"))
+
+function prob2Gio(filename::String)
+    sum = 0
+    alphabet = 'a':'z'
+    dict_l = Dict(alphabet[i] => i for i in 1:26)
+
+    f = open(filename)
+    l = readlines(f)
+    triplets = [l[i:(i+2)] for i in 1:3:length(l)]
+    for el in triplets
+        i = intersect(el[1], el[2], el[3])[1]
+        sum += (dict_l[lowercase(i)] + 26 * isuppercase(i))
+    end
+    return sum
+end
+
+print(prob2Gio("input_3.txt"))

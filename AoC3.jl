101e23c82fab0b2289c2c2b104c79b0f7578cace
Giovanni Romanò <3172379@phd.unibocconi.it>
2022-12-03 17:10:16 +0100

    Sistemato due casini che si erano creati

--- AoC3.jl
+++ AoC3.jl
@@ -3,21 +3,19 @@ Pkg.activate(".")
 Pkg.instantiate()
 
 
-function halfslice(a::Array)
-    [a[1:(end ÷ 2)], a[(end ÷ 2):end]]
 function halfslice(a::String)
     [a[1:(end ÷ 2)], a[(end ÷ 2 + 1):end]]
 end
 
 function prob1Gio(filename::String)
+    sum = 0
+    f = open(filename)
     alphabet = 'a':'z'
     dict_l = Dict(alphabet[i] => i for i in 1:26)
     for ln in eachline(f)
-        h = halfslice(split(ln, ""))
         h = halfslice(ln)
         i = intersect(h[1], h[2])
         for el in i
-            sum += dict_l[only(lowercase(el))] + 26 * isuppercase(only(el))
             sum += (dict_l[only(lowercase(el))] + 26 * isuppercase(only(el)))
 
         end

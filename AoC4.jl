aeaad792e8f4e29a8c028ac1c9d3757044a5da60
Giovanni Romanò <3172379@phd.unibocconi.it>
2022-12-04 18:31:51 +0100

    fix p1d4G and add p1d4G

--- AoC4.jl
+++ AoC4.jl
@@ -1,11 +1,25 @@
-function isincluded(s::String)
+function isfullyincluded(s::String)
     return reduce(*, reduce(-, map(x -> parse.(Int, x), split.(split(s, ","), "-")))) <= 0
 end
 
 function prob1Gio(filename::String)
     open(filename) do f
-        return sum(isincluded.(readlines(f)))
+        return sum(isfullyincluded.(readlines(f)))
     end
 end
 
 prob1Gio("input_4.txt")
+
+function prob2Gio(filename::String)
+    f = open(filename)
+    count = 0
+    for ln in eachline(f)
+        m = map(x -> parse.(Int, x), split.(split(ln, ","), "-"))
+        sort!(m)
+        # I need to sort!(m) so that also the full inclusion is counted
+        count += (reduce(*, m[1] .- m[2][1]) <= 0)
+    end
+    return count
+end
+
+prob2Gio("input_4.txt")
